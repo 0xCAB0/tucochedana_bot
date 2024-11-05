21b5a323014020481066eb631b34fa6188f46971
Álvaro Cabo Ciudad <alvarocaboac2@gmail.com>
2024-11-05 19:03:02 +0100

    Added Makefile, docker-compose and support for Postgres env

---
+++ Makefile
@@ -0,0 +1,17 @@
+include .env
+
+db:
+	docker run --rm -d --name postgres -p 5432:5432 \
+  -e POSTGRES_DB=$(POSTGRES_DB) \
+  -e POSTGRES_USER=$(POSTGRES_USER) \
+  -e POSTGRES_PASSWORD=$(POSTGRES_PASSWORD) \
+  postgres:latest
+
+
+diesel:
+	DATABASE_URL=postgres://$(POSTGRES_USER):$(POSTGRES_PASSWORD)@localhost/$(POSTGRES_DB) diesel migration run
+stop:
+	docker kill postgres
+
+clippy:
+	cargo clippy --all-features
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
-use std::fmt::Debug;
 use db::BotDbError;
+use fang::{FangError, ToFangError};
 use lazy_static::lazy_static;
+use std::fmt::Debug;
 use telegram::client::ApiError;
 use thiserror::Error;
-use fang::{ToFangError, FangError};
-lazy_static!{
+lazy_static! {
     pub static ref TELEGRAM_BOT_TOKEN: String =
         std::env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
-    pub static ref DATABASE_URL: String =
-        std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
-    pub static ref API_URL: String = 
+    pub static ref POSTGRES_DB: String =
+        std::env::var("POSTGRES_DB").unwrap_or(String::from("vinted-rs"));
+    pub static ref POSTGRES_USER: String =
+        std::env::var("POSTGRES_USER").unwrap_or(String::from("postgres"));
+    pub static ref POSTGRES_PASSWORD: String =
+        std::env::var("POSTGRES_PASSWORD").unwrap_or(String::from("postgres"));
+    pub static ref DB_URI: String = {
+            dotenvy::dotenv().ok(); // Load environment variables from .env file
+            format!(
+                "postgres://{}:{}@localhost/{}?sslmode=disable",
+                *POSTGRES_USER, *POSTGRES_PASSWORD, *POSTGRES_DB
+            )
+        };
+    pub static ref API_URL: String =
         std::env::var("API_URL").expect("API_URL not set");
-    pub static ref BOT_NAME: String = 
+    pub static ref BOT_NAME: String =
         std::env::var("BOT_NAME").expect("BOT_NAME not set");
 }
 
-pub mod tucochedana{
+pub mod tucochedana {
     pub mod client;
 }
 
