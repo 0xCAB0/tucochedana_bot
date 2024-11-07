9262e32f0d431bd4a0e32531e7038929fc5925f2
Álvaro Cabo Ciudad <alvarocaboac2@gmail.com>
2024-11-07 10:54:57 +0100

    Improved api

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
+use std::net::SocketAddr;
+
 use axum::{
+    extract::ConnectInfo,
     http::StatusCode,
     routing::{get, post},
     Json, Router,
@@ -9,9 +12,14 @@ use crate::update_handler::process_update_task::ProcessUpdateTask;
 
 pub fn app() -> Router {
     Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
+        .route("/", get(handle_root))
         .route("/webhook", post(parse_update))
 }
+
+async fn handle_root(ConnectInfo(addr): ConnectInfo<SocketAddr>) -> String {
+    format!("Hello! Your IP address is: {}", addr.ip())
+}
+
 async fn parse_update(update: Json<Update>) -> (StatusCode, Json<()>) {
     log::info!("New update {:?}", update);
     ProcessUpdateTask::new(update.0);
