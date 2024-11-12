85ac2ed36d44d8b1cf70eb90a02ef48832ca8e2e
Álvaro Cabo Ciudad <alvarocaboac2@gmail.com>
2024-11-12 18:08:29 +0100

    Should fix get_vehicles

--- src/db/queries/get_vehicles.sql
+++ src/db/queries/get_vehicles.sql
@@ -2,4 +2,9 @@
 -- string_to_array($1, ',') converts this string into an array {ABC123, DEF456, GHI789}.
 -- ANY() checks if plate is in this array.
 
-SELECT * FROM vehicles WHERE plate = ANY (string_to_array($1, ','));
+SELECT *
+FROM vehicles
+WHERE
+    plate IN (
+        SELECT UNNEST(string_to_array($1, ','))
+    );
